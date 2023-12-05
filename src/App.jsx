92a47bea38a4badd7e6564393c45b60aa3373f43
silviosalza <silviofilipposalza@gmail.com>
2023-12-05 16:04:10 +0100

    created form and initial useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //setto stato iniziale del mio input
+  const [title, setTitle] = useState("");
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="py-5 ">
+        <div className="container mx-auto">
+          <h1 className="font-bold">Titolo dell'articolo</h1>
+          <form action="" className="flex flex-col gap-2 w-1/2">
+            <label htmlFor="article_title"></label>
+            <input
+              className="rounded border-2 border-black"
+              type="text"
+              name="title"
+              placeholder="Inserisci il titolo dell'articolo"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <button className="bg-green-300 hover:bg-green-400 rounded border-2 border-black">
+              Crea
+            </button>
+          </form>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
